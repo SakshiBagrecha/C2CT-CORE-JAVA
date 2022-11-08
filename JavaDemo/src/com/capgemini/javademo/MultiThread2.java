6f6f8e8414aa3bfab13774c9cc92928384406b65
SAKSHI <SAKSHI@LAPTOP-29VCUL7Q>
2022-11-08 21:49:01 +0530

    Second Commit

---
+++ JavaDemo/src/com/capgemini/javademo/MultiThread2.java
@@ -0,0 +1,36 @@
+package com.capgemini.javademo;
+
+
+
+class Eclipse extends Thread{
+	
+	public void run()
+	{
+	    
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Eclipse Thread name is" + Thread.currentThread().getName());
+			
+			try {
+				sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+}
+
+public class MultiThread2 {
+	
+	public static void main(String[] args) {
+		Eclipse e=new Eclipse();
+		e.start();
+		
+		Eclipse e1=new Eclipse();
+		e1.start();
+		
+		   
+		}
+}
+
